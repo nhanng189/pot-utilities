cb70d90950b65207d174d754d1ab5ea148d166cc
nhannl <nhan@c2c-techhub.io>
2021-03-17 10:53:39 +0700

    Update due to landing page

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import "../styles/globals.scss";
+import "../styles/animation.scss";
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: (theme) => ({
+        "shape-pattern": "url('/animated-shape.svg')",
+      }),
+    },
   },
   variants: {
     extend: {
